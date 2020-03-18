71174c69460b5b2290c1f16a139f8d3845c50f36
xuzhan <xuzhan@tencent.com>
2020-03-18 12:23:00 +0800

    feature: 修改big-select下拉框样式，修改遮罩mask issue #893

--- src/frontend/devops-nav/src/components/Header/index.vue
+++ src/frontend/devops-nav/src/components/Header/index.vue
@@ -303,6 +303,7 @@
                 }
                 .bk-select-angle {
                     color: white;
+                    top: 7px;
                 }
                 .bk-tooltip-ref {
                     outline: none;
