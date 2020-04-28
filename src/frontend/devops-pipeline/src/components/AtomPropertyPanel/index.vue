305d7daa07e6a2649fed4c7ffa23f87f82e6eb51
terlinhe <terlinhe@tencent.com>
2020-04-28 17:49:42 +0800

    story:自定义插件codecc区分v2版本和v3版本 --story=858130213

--- src/frontend/devops-pipeline/src/components/AtomPropertyPanel/index.vue
+++ src/frontend/devops-pipeline/src/components/AtomPropertyPanel/index.vue
@@ -74,6 +74,7 @@
                         <div
                             v-if="atom.atomModal"
                             :is="AtomComponent"
+                            :atom="atom.atomModal"
                             :element-index="elementIndex"
                             :container-index="containerIndex"
                             :stage-index="stageIndex"
