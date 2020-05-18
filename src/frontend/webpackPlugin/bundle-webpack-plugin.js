f3edf759337f143ca29f874b0b85c3221eb8bc1c
lockiechen <lockiechen@tencent.com>
2020-05-18 15:50:06 +0800

    :feature 动态生成前端资源文件json --story=858350831

--- src/frontend/webpackPlugin/bundle-webpack-plugin.js
+++ src/frontend/webpackPlugin/bundle-webpack-plugin.js
@@ -18,72 +18,109 @@
  */
 const fs = require('fs')
 const path = require('path')
+const fetch = require('node-fetch')
+const chalk = require('chalk')
+
+async function getAssetsJSON (jsonUrl) {
+    try {
+        const res = await fetch(jsonUrl)
+        const assets = await res.json()
+
+        console.log(chalk.blue.bold(`Successfully get assets json from ${jsonUrl}!`))
+        console.table(assets)
+        return assets
+    } catch (error) {
+        console.log(chalk.yellow.bgRed.bold(`Failed get assets json from ${jsonUrl}!`))
+        return {}
+    }
+}
 
 module.exports = class BundleWebpackPlugin {
     // Define `apply` as its prototype method which is supplied with compiler as its argument
     constructor (props) {
-      const dist = props.dist || '.'
-      const bundleName = props.bundleName || 'assets_bundle'
-      this.SERVICE_ASSETS_JSON_PATH = path.join(__dirname, '..', dist, `${bundleName}.json`)
-      this.SERVICE_ASSETS_DIR = path.dirname(this.SERVICE_ASSETS_JSON_PATH)
+        const dist = props.dist || '.'
+        const envPrefix = props.envPrefix ? `${props.envPrefix}.` : ''
+        const bundleName = props.bundleName || 'assets_bundle'
+        this.SERVICE_ASSETS_JSON_PATH = path.join(
+            __dirname,
+            '..',
+            dist,
+            `${bundleName}.json`
+        )
+        this.ASSETS_JSON_URL = `http://${envPrefix}devops.oa.com/${bundleName}.json`
+        this.SERVICE_ASSETS_DIR = path.dirname(this.SERVICE_ASSETS_JSON_PATH)
     }
 
-    apply(compiler) {
-      compiler.hooks.done.tapAsync(
-        'BundleWebpackPlugin',
-        (compilation, callback) => {
-          console.log('This is an example plugin!');
-          const { SERVICE_ASSETS_JSON_PATH, SERVICE_ASSETS_DIR } = this
-          const entryNames = Array.from(compilation.compilation.entrypoints.keys())    
-          const extensionRegexp = /\.(css|js|mjs)(\?|$)/;
-          const entryPointPublicPathMap = {}
-          let assetsMap = {}
+    apply (compiler) {
+        compiler.hooks.done.tapAsync(
+            'BundleWebpackPlugin',
+            async (compilation, callback) => {
+                console.log('This is an example plugin!')
+                const { SERVICE_ASSETS_JSON_PATH, SERVICE_ASSETS_DIR, ASSETS_JSON_URL } = this
+                const entryNames = Array.from(
+                    compilation.compilation.entrypoints.keys()
+                )
+                const extensionRegexp = /\.(css|js|mjs)(\?|$)/
+                const entryPointPublicPathMap = {}
+                const assetsMap = {}
 
-          for (let i = 0; i < entryNames.length; i++) {
-              const entryName = entryNames[i];
-              const entryPointFiles = compilation.compilation.entrypoints.get(entryName).getFiles();
-              let assets = {
-                js: [],
-                css: []
-              }
-              entryPointFiles
-                .map(chunkFile => chunkFile.split('/').map(encodeURIComponent).join('/'))
-                .map(entryPointPublicPath => {
+                for (let i = 0; i < entryNames.length; i++) {
+                    const entryName = entryNames[i]
+                    const entryPointFiles = compilation.compilation.entrypoints
+                        .get(entryName)
+                        .getFiles()
+                    const assets = {
+                        js: [],
+                        css: []
+                    }
+                    entryPointFiles
+                        .map((chunkFile) =>
+                            chunkFile
+                                .split('/')
+                                .map(encodeURIComponent)
+                                .join('/')
+                        )
+                        .map(entryPointPublicPath => {
+                            const extMatch = extensionRegexp.exec(
+                                entryPointPublicPath
+                            )
+                            // Skip if the public path is not a .css, .mjs or .js file
+                            if (!extMatch) {
+                                return
+                            }
+                            // Skip if this file is already known
+                            // (e.g. because of common chunk optimizations)
+                            if (entryPointPublicPathMap[entryPointPublicPath]) {
+                                return
+                            }
+                            entryPointPublicPathMap[entryPointPublicPath] = true
+                            // ext will contain .js or .css, because .mjs recognizes as .js
+                            const ext = extMatch[1] === 'mjs' ? 'js' : extMatch[1]
+                            // 服务均为SPA单入口文件
+                            console.log(entryPointFiles, entryPointPublicPath)
+                            assets[ext] = entryPointPublicPath
+                        })
                     
-                    const extMatch = extensionRegexp.exec(entryPointPublicPath);
-                    // Skip if the public path is not a .css, .mjs or .js file
-                    if (!extMatch) {
-                      return;
-                    }
-                    // Skip if this file is already known
-                    // (e.g. because of common chunk optimizations)
-                    if (entryPointPublicPathMap[entryPointPublicPath]) {
-                      return;
-                    }
-                    entryPointPublicPathMap[entryPointPublicPath] = true;
-                    // ext will contain .js or .css, because .mjs recognizes as .js
-                    const ext = extMatch[1] === 'mjs' ? 'js' : extMatch[1];
-                    assets[ext].push(entryPointPublicPath);
-                })
-                
-                assetsMap[entryName] = assets
-          }
+                    assetsMap[entryName] = assets
+                }
 
-          let json = {}
-          if (fs.existsSync(SERVICE_ASSETS_JSON_PATH)) {
-            json = JSON.parse(fs.readFileSync(SERVICE_ASSETS_JSON_PATH).toString())            
-          }
-          json = {
-            ...json,
-            ...assetsMap
-          }
-          if (!fs.existsSync(SERVICE_ASSETS_DIR)) {
-            fs.mkdirSync(SERVICE_ASSETS_DIR)
-          }
-          fs.writeFileSync(SERVICE_ASSETS_JSON_PATH, JSON.stringify(json))
-          
-          callback();
-        }
-      );
+                const assetJSON = await getAssetsJSON(ASSETS_JSON_URL)
+                let json = {}
+                if (fs.existsSync(SERVICE_ASSETS_JSON_PATH)) {
+                    json = require(SERVICE_ASSETS_JSON_PATH)
+                }
+                json = {
+                    ...assetJSON,
+                    ...json,
+                    ...assetsMap
+                }
+                if (!fs.existsSync(SERVICE_ASSETS_DIR)) {
+                    fs.mkdirSync(SERVICE_ASSETS_DIR)
+                }
+                fs.writeFileSync(SERVICE_ASSETS_JSON_PATH, JSON.stringify(json))
+                console.table(json)
+                callback()
+            }
+        )
     }
-  }
+}
