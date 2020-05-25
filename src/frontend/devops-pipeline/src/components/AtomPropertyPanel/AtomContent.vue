4be1895a788df12473c032643bd4a38e2da18459
xuzhan <xuzhan@tencent.com>
2020-05-25 11:22:47 +0800

    feature: 流水线支持暂停 issue #1260 - 修改接受通知文案 - 暂停编辑时不能编辑版本和选择插件 - 点击继续或者暂停后，调用详情接口再取消暂停

--- src/frontend/devops-pipeline/src/components/AtomPropertyPanel/AtomContent.vue
+++ src/frontend/devops-pipeline/src/components/AtomPropertyPanel/AtomContent.vue
@@ -14,7 +14,7 @@
                         <div class="atom-select-entry">
                             <template v-if="atom">
                                 <span :title="atom.recommendFlag === false ? $t('editPage.notRecomendPlugin') : atom.name" :class="[{ 'not-recommend': atom.recommendFlag === false }, 'atom-selected-name']">{{ atom.name }}</span>
-                                <bk-button theme="primary" class="atom-select-btn reselect-btn" :disabled="!editable" @click.stop="toggleAtomSelectorPopup(true)">{{ $t('editPage.reSelect') }}</bk-button>
+                                <bk-button theme="primary" class="atom-select-btn reselect-btn" :disabled="!editable || showPanelType === 'PAUSE'" @click.stop="toggleAtomSelectorPopup(true)">{{ $t('editPage.reSelect') }}</bk-button>
                             </template>
                             <template v-else-if="!atomCode">
                                 <bk-button theme="primary" class="atom-select-btn" @click.stop="toggleAtomSelectorPopup(true)">{{ $t('editPage.selectAtomTips') }}</bk-button>
@@ -30,7 +30,7 @@
                         :placeholder="$t('editPage.selectAtomVersion')"
                         name="version"
                         @selected="handleUpdateVersion"
-                        :disabled="!editable"
+                        :disabled="!editable || showPanelType === 'PAUSE'"
                     >
                         <bk-option v-for="v in atomVersionList" :key="v.versionName" :id="v.versionValue" :name="v.versionName"></bk-option>
                     </bk-select>
@@ -455,7 +455,8 @@
                 'fetchAtomModal',
                 'fetchAtomVersionList',
                 'togglePropertyPanel',
-                'pausePlugin'
+                'pausePlugin',
+                'requestPipelineExecDetail'
             ]),
 
             ...mapActions('soda', [
@@ -474,7 +475,9 @@
                     element: this.element
                 }
                 this[loadingKey] = true
-                this.pausePlugin(postData).catch((err) => {
+                this.pausePlugin(postData).then(() => {
+                    return this.requestPipelineExecDetail(this.$route.params)
+                }).catch((err) => {
                     this.$showTips({
                         message: err.message || err,
                         theme: 'error'
