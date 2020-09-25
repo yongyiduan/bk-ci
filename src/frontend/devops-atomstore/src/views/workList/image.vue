9007b4800ac90780da478c18c1d924a8fbfeb063
xuzhan <xuzhan@tencent.com>
2020-09-25 16:43:55 +0800

    task: select组件支持虚拟滚动，解决大数据性能问题 issue #1923 #1922 - 商店镜像选择项目支持大数据

--- src/frontend/devops-atomstore/src/views/workList/image.vue
+++ src/frontend/devops-atomstore/src/views/workList/image.vue
@@ -116,11 +116,21 @@
                         :rules="[requireRule]"
                         error-display-type="normal"
                     >
-                        <bk-select v-model="relateImageData.form.projectCode" searchable :placeholder="$t('store.请选择项目')" @change="toggleProjectList">
-                            <bk-option v-for="option in projectList"
-                                :key="option.projectCode"
-                                :id="option.projectCode"
-                                :name="option.projectName">
+                        <bk-select v-model="relateImageData.form.projectCode"
+                            @change="toggleProjectList"
+                            searchable
+                            :placeholder="$t('store.请选择项目')"
+                            :enable-virtual-scroll="projectList && projectList.length > 3000"
+                            :list="projectList"
+                            id-key="projectCode"
+                            display-key="projectName"
+                        >
+                            <bk-option
+                                v-for="item in projectList"
+                                :key="item.projectCode"
+                                :id="item.projectCode"
+                                :name="item.projectName"
+                            >
                             </bk-option>
                             <a href="/console/pm" slot="extension" target="_blank"> {{ $t('store.新增项目') }} </a>
                         </bk-select>
