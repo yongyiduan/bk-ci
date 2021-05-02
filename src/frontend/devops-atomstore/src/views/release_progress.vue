a4d37340e3ef28b9084a13b6a65001d475eb2754
xuzhan <xuzhan@tencent.com>
2021-05-02 14:55:45 +0800

    feature: 插件升级时检验发布类型是否正确 issue #3875

--- src/frontend/devops-atomstore/src/views/release_progress.vue
+++ src/frontend/devops-atomstore/src/views/release_progress.vue
@@ -459,7 +459,7 @@
                 }
             },
 
-            async rebuild () {
+            async rebuild (fieldCheckConfirmFlag) {
                 if (!this.permission) return
 
                 let message, theme
@@ -468,20 +468,25 @@
                     await this.$store.dispatch('store/rebuild', {
                         atomId: this.routerParams.atomId,
                         projectId: this.versionDetail.projectCode,
-                        initProject: this.versionDetail.initProjectCode
+                        initProject: this.versionDetail.initProjectCode,
+                        fieldCheckConfirmFlag
                     })
 
                     message = this.$t('store.操作成功')
                     theme = 'success'
                     // this.requestRelease(this.routerParams.atomId)
                 } catch (err) {
+                    if ([2120030, 2120031].includes(err.status)) {
+                        this.confirmSubmit(err.message, () => this.rebuild(true))
+                        return
+                    }
+
                     message = err.message ? err.message : err
                     theme = 'error'
                 } finally {
-                    this.$bkMessage({
-                        message,
-                        theme
-                    })
+                    if (message) {
+                        this.$bkMessage({ message, theme })
+                    }
                 }
             },
             readLog () {
