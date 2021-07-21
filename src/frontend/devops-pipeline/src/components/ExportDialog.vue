4a1f1cde2d4b08887efb73823d7a28bdb2380e3f
xuzhan <xuzhan@tencent.com>
2021-07-21 17:33:55 +0800

    feature: 修改流水线导出的下载方式 #4738

--- src/frontend/devops-pipeline/src/components/ExportDialog.vue
+++ src/frontend/devops-pipeline/src/components/ExportDialog.vue
@@ -64,7 +64,7 @@
                     {
                         title: 'PreCI',
                         icon: 'export-prebuild',
-                        name: 'prebuild.json',
+                        name: `${this.pipelineName}.yml`,
                         tips: this.$t('newlist.exportYamlTip'),
                         exportUrl: `${API_URL_PREFIX}/${PROCESS_API_URL_PREFIX}/user/pipelines/${this.pipelineId}/projects/${this.projectId}/yaml/prebuild`,
                         tipsLink: 'https://iwiki.woa.com/x/ruhACw'
@@ -73,7 +73,7 @@
                     {
                         title: '工蜂CI Yaml',
                         icon: 'export-ci',
-                        name: 'stream.yml',
+                        name: `${this.pipelineName}.yml`,
                         tips: this.$t('newlist.exportGitciTip'),
                         exportUrl: `${API_URL_PREFIX}/${PROCESS_API_URL_PREFIX}/user/pipelines/${this.pipelineId}/projects/${this.projectId}/yaml/gitci`,
                         tipsLink: 'https://iwiki.woa.com/x/QBcLAQ'
