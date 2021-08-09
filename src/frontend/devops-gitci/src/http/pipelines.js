7dad72b84c24a222c5617073fe05e6f9a462cd91
xuzhan <xuzhan@tencent.com>
2021-08-09 19:30:22 +0800

    feature: 质量红线和审核流 issue #865860343

--- src/frontend/devops-gitci/src/http/pipelines.js
+++ src/frontend/devops-gitci/src/http/pipelines.js
@@ -168,7 +168,7 @@ export default {
         return api.post(`${QUALITY_PREFIX}/user/intercepts/v2/pipeline/list?projectId=${projectId}&pipelineId=${pipelineId}&buildId=${buildId}`, ids)
     },
 
-    triggerStage ({ projectId, pipelineId, buildNo, stageId, cancel, reviewParams, id, suggest }) {
-        return api.post(`${PROCESS_PREFIX}/user/builds/projects/${projectId}/pipelines/${pipelineId}/builds/${buildNo}/stages/${stageId}/manualStart?cancel=${cancel}`, { reviewParams, id, suggest })
+    triggerStage ({ projectId, pipelineId, buildId, stageId, cancel, reviewParams, id, suggest }) {
+        return api.post(`${PROCESS_PREFIX}/user/builds/projects/${projectId}/pipelines/${pipelineId}/builds/${buildId}/stages/${stageId}/manualStart?cancel=${cancel}`, { reviewParams, id, suggest })
     }
 }
