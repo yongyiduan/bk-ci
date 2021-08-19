b7ee6f26be465c594529fa6f2b2b88a41f3e7361
xuzhan <xuzhan@tencent.com>
2021-08-19 23:23:42 +0800

    feature: 质量红线和审核流 issue #865860343

--- src/frontend/devops-gitci/src/components/StageReviewPanel/components/params/approve.vue
+++ src/frontend/devops-gitci/src/components/StageReviewPanel/components/params/approve.vue
@@ -65,6 +65,7 @@
                     const errorKeys = []
                     this.params.forEach(({ required, valueType, value, key }) => {
                         if (required) {
+                            key = key.replace(/^variables\./, '')
                             if (typeof value === 'undefined' || value === '') {
                                 errorKeys.push(key)
                             }
