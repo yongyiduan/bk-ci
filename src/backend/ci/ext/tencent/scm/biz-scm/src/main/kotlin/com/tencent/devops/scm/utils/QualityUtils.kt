dca58b274a203906d83f68161d5dd34c2a2e2f6f
ruotiantang <ruotiantang@tencent.com>
2021-10-15 14:55:52 +0800

    --story=868010731 当mr触发且配置了红线时，评论信息格式优化

--- src/backend/ci/ext/tencent/scm/biz-scm/src/main/kotlin/com/tencent/devops/scm/utils/QualityUtils.kt
+++ src/backend/ci/ext/tencent/scm/biz-scm/src/main/kotlin/com/tencent/devops/scm/utils/QualityUtils.kt
@@ -29,52 +29,44 @@ package com.tencent.devops.scm.utils
 
 object QualityUtils {
     fun getQualityReport(titleData: List<String>, resultData: MutableMap<String, MutableList<List<String>>>): String {
+        val sb = StringBuilder()
         val (status, timeCost, triggerType, pipelineName, url) = titleData
         val pipelineNameTitle = if (titleData.size >= 6) {
             titleData[5]
         } else {
             "蓝盾流水线"
         }
+        sb.append("$pipelineNameTitle：[$pipelineName]($url) \n")
 
-        // 生成报表
-        val statusLine = if (status == "SUCCEED") "<td style=\"color:#4CAF50;border:none;font-weight: bold;padding-left:0;\">执行成功</td>"
-                                else "<td style=\"color:red;border:none;font-weight: bold;padding-left:0;\">执行失败</td>"
-        val title = "<table><tr>" +
-                "<td style=\"border:none;padding-right: 0;\">$pipelineNameTitle：</td>" +
-                "<td style=\"border:none;padding-left:0;\"><a href='$url' style=\"color: #03A9F4\">$pipelineName</a></td>" +
-                "<td style=\"border:none;padding-right: 0;\">状态：</td>" +
-                statusLine +
-                "<td style=\"border:none;padding-right: 0\">触发方式：</td>" +
-                "<td style=\"border:none;padding-left:0;\">$triggerType</td>" +
-                "<td style=\"border:none;padding-right: 0\">任务耗时：</td>" +
-                "<td style=\"border:none;padding-left:0;\">$timeCost</td>" +
-                "</tr></table>"
-        val body = StringBuilder("")
-        body.append("<table border=\"1\" cellspacing=\"0\" width=\"450\">")
-        body.append("<tr>")
-        body.append("<th style=\"text-align:left;\">质量红线产出插件</th>")
-        body.append("<th style=\"text-align:left;\">指标</th>")
-        body.append("<th style=\"text-align:left;\">结果</th>")
-        body.append("<th style=\"text-align:left;\">预期</th>")
-        body.append("<th style=\"text-align:left;\"></th>")
-        body.append("</tr>")
-        resultData.forEach { elementName, result ->
+        val statusLine = if (status == "SUCCEED") {
+            "<font color=#4CAF50>执行成功</font>"
+        } else {
+            "<font color=red>执行成功</font>"
+        }
+        sb.append("状态：$statusLine \n")
+
+        sb.append("触发方式：$triggerType \n")
+
+        sb.append("任务耗时：$timeCost \n")
+
+        sb.append("| 质量红线产出插件 | 指标 | 结果 | 预期 |  | \n")
+        sb.append("| :----:| :----: | :----: | :----: | :----: | \n")
+        resultData.forEach { (elementName, result) ->
             result.forEachIndexed { index, list ->
-                body.append("<tr>")
-                if (index == 0) body.append("<td>$elementName</td>") else body.append("<td></td>")
-                body.append("<td>${list[0]}</td>")
-                body.append("<td>${list[1]}</td>")
-                body.append("<td>${list[2]}</td>")
-                if (list[3] == "true") {
-                    body.append("<td style=\"color: #4CAF50; font-weight: bold;\">&nbsp; &radic; &nbsp;</td>")
+                val prefix = if (index == 0) {
+                    "| $elementName "
                 } else {
-                    body.append("<td style=\"color: #F44336; font-weight: bold;\">&nbsp; &times; &nbsp;</td>")
+                    "| "
                 }
-                body.append("</tr>")
+                val (target, outcome, expected, state) = list
+                val pattern = if (state == "true") {
+                    "<font color=#4CAF50> &nbsp; &radic; &nbsp; </font>"
+                } else {
+                    "<font color=#F44336> &nbsp; &times; &nbsp; </font>"
+                }
+                sb.append("$prefix | $target | $outcome | $expected | $pattern | \n")
             }
         }
-        body.append("</table>")
-
-        return title + body.toString()
+        return sb.toString()
     }
 }
