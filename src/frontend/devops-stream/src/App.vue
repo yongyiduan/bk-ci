4a40122bb98aec0c6f1561a2efc1cdb94872d0ea
xuzhan <xuzhan@tencent.com>
2021-11-24 16:38:43 +0800

    --story=869623627 前端设置cookie现在统一往 *.woa.com、*.oa.com写，会导致X-DEVOPS-PROJECT-ID被污染

--- src/frontend/devops-stream/src/App.vue
+++ src/frontend/devops-stream/src/App.vue
@@ -38,8 +38,11 @@
                     name: routeName
                 })
             }
+            // 清除异常cookie
             deleteCookie(X_DEVOPS_PROJECT_ID, 'oa.com')
             deleteCookie(X_DEVOPS_PROJECT_ID, 'woa.com')
+            deleteCookie(ROUTER_TAG, 'oa.com')
+            deleteCookie(ROUTER_TAG, 'woa.com')
         }
     }
 </script>
