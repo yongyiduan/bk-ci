8fc169a1b287e07243e0c27b073419edcb295b71
irwinsun <irwinsun@tencent.com>
2022-06-29 11:40:42 +0800

    bug: final stage终止可能出现执行成功的状态 #7023
    找回重构导致的逻辑丢失： runtime 覆盖 system 环境变量

--- src/backend/ci/core/worker/worker-common/src/main/kotlin/com/tencent/devops/worker/common/task/market/MarketAtomTask.kt
+++ src/backend/ci/core/worker/worker-common/src/main/kotlin/com/tencent/devops/worker/common/task/market/MarketAtomTask.kt
@@ -222,6 +222,7 @@ open class MarketAtomTask : ITask() {
                 JAVA_PATH_ENV to getJavaFile().absolutePath
             )
         ).toMutableMap()
+        logger.info("runtimeVariables=$runtimeVariables")
 
         var error: Throwable? = null
         try {
@@ -249,7 +250,9 @@ open class MarketAtomTask : ITask() {
             }
 
             // #7023 找回重构导致的逻辑丢失： runtime 覆盖 system 环境变量
-//            systemEnvVariables.forEach { runtimeVariables.putIfAbsent(it.key, it.value) }
+            systemEnvVariables.forEach { runtimeVariables.putIfAbsent(it.key, it.value) }
+
+            logger.info("runtimeVariables_add_system=$runtimeVariables")
 
             val preCmd = atomData.preCmd
             val buildEnvs = buildVariables.buildEnvs
