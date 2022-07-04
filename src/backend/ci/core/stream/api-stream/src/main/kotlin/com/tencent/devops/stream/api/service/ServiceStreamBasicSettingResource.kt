260e62d51d8a9badbe1d080d93307ac21d8867a8
mingshewhe <mingshewhe@tencent.com>
2022-07-04 17:38:33 +0800

    feat: Stream腾讯云部署 #7028

--- src/backend/ci/core/stream/api-stream/src/main/kotlin/com/tencent/devops/stream/api/service/ServiceStreamBasicSettingResource.kt
+++ src/backend/ci/core/stream/api-stream/src/main/kotlin/com/tencent/devops/stream/api/service/ServiceStreamBasicSettingResource.kt
@@ -27,8 +27,6 @@
 
 package com.tencent.devops.stream.api.service
 
-import com.tencent.devops.common.api.auth.AUTH_HEADER_USER_ID
-import com.tencent.devops.common.api.auth.AUTH_HEADER_USER_ID_DEFAULT_VALUE
 import com.tencent.devops.common.api.pojo.Result
 import com.tencent.devops.stream.pojo.StreamBasicSetting
 import io.swagger.annotations.Api
@@ -36,7 +34,6 @@ import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
 import javax.ws.rs.Consumes
 import javax.ws.rs.GET
-import javax.ws.rs.HeaderParam
 import javax.ws.rs.Path
 import javax.ws.rs.PathParam
 import javax.ws.rs.Produces
@@ -52,9 +49,6 @@ interface ServiceStreamBasicSettingResource {
     @GET
     @Path("/{projectId}")
     fun getStreamConf(
-        @ApiParam(value = "用户ID", required = true, defaultValue = AUTH_HEADER_USER_ID_DEFAULT_VALUE)
-        @HeaderParam(AUTH_HEADER_USER_ID)
-        userId: String,
         @ApiParam(value = "蓝盾项目ID", required = true)
         @PathParam("projectId")
         projectId: String
