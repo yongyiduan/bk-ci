2f759bacf9ba58680d27a10c9e9c9c0f2a0eb54e
yongyiduan <yongyiduan@tencent.com>
2022-07-13 11:39:58 +0800

    Stream腾讯云部署 #7028 解决冲突

--- src/backend/ci/core/stream/biz-stream/src/main/kotlin/com/tencent/devops/stream/trigger/parsers/MergeConflictCheck.kt
+++ src/backend/ci/core/stream/biz-stream/src/main/kotlin/com/tencent/devops/stream/trigger/parsers/MergeConflictCheck.kt
@@ -77,7 +77,6 @@ class MergeConflictCheck @Autowired constructor(
         path2PipelineExists: Map<String, StreamTriggerPipeline>
     ): Boolean {
         val projectId = action.data.eventCommon.gitProjectId
-        val mrRequestId = action.event().object_attributes.id.toString()
 
         val mrInfo = action.api.getMrInfo(
             gitProjectId = action.getGitProjectIdOrName(projectId),
@@ -146,7 +145,8 @@ class MergeConflictCheck @Autowired constructor(
         var isFinish: Boolean
         var isTrigger: Boolean
         val projectId = action.data.eventCommon.gitProjectId
-        val mrRequestId = action.event().object_attributes.id.toString()
+        val event = action.event() as GitMergeRequestEvent
+        val mrRequestId = event.object_attributes.id.toString()
         val mrInfo = try {
             action.api.getMrInfo(
                 gitProjectId = action.getGitProjectIdOrName(projectId),
