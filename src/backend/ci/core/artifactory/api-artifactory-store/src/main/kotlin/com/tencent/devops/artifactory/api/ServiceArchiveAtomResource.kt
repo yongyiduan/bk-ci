9f8d525a799863eff812858735fd0fa217aecabc
carlyin <449036249@qq.com>
2022-09-23 14:43:32 +0800

    test

--- src/backend/ci/core/artifactory/api-artifactory-store/src/main/kotlin/com/tencent/devops/artifactory/api/ServiceArchiveAtomResource.kt
+++ src/backend/ci/core/artifactory/api-artifactory-store/src/main/kotlin/com/tencent/devops/artifactory/api/ServiceArchiveAtomResource.kt
@@ -27,15 +27,21 @@
 
 package com.tencent.devops.artifactory.api
 
+import com.tencent.devops.artifactory.pojo.ArchiveAtomResponse
 import com.tencent.devops.common.api.auth.AUTH_HEADER_USER_ID
 import com.tencent.devops.common.api.pojo.Result
+import com.tencent.devops.store.pojo.common.enums.ReleaseTypeEnum
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition
+import org.glassfish.jersey.media.multipart.FormDataParam
+import java.io.InputStream
 import javax.ws.rs.Consumes
 import javax.ws.rs.DELETE
 import javax.ws.rs.GET
 import javax.ws.rs.HeaderParam
+import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.QueryParam
@@ -70,4 +76,37 @@ interface ServiceArchiveAtomResource {
         @QueryParam("atomCode")
         atomCode: String
     ): Result<Boolean>
+
+    @ApiOperation("归档插件包资源")
+    @POST
+    @Path("/archiveAtom")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    fun archiveAtomFile(
+        @ApiParam("userId", required = true)
+        @QueryParam("userId")
+        userId: String,
+        @ApiParam("项目编码", required = true)
+        @QueryParam("projectCode")
+        projectCode: String,
+        @ApiParam("插件ID", required = true)
+        @QueryParam("atomId")
+        atomId: String,
+        @ApiParam("插件代码", required = true)
+        @QueryParam("atomCode")
+        atomCode: String,
+        @ApiParam("插件版本号", required = true)
+        @QueryParam("version")
+        version: String,
+        @ApiParam("发布类型", required = true)
+        @QueryParam("releaseType")
+        releaseType: ReleaseTypeEnum,
+        @ApiParam("文件", required = true)
+        @FormDataParam("file")
+        inputStream: InputStream,
+        @FormDataParam("file")
+        disposition: FormDataContentDisposition,
+        @ApiParam("支持的操作系统", required = true)
+        @QueryParam("os")
+        os: String
+    ): Result<ArchiveAtomResponse?>
 }
