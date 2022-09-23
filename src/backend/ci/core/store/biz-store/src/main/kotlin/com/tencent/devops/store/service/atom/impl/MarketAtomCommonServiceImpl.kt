76eba7d3824b21cdd2a09d545ed512b51e1285c4
yjieliang <yundantengteng@163.com>
2022-09-23 21:07:07 +0800

    test

--- src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/service/atom/impl/MarketAtomCommonServiceImpl.kt
+++ src/backend/ci/core/store/biz-store/src/main/kotlin/com/tencent/devops/store/service/atom/impl/MarketAtomCommonServiceImpl.kt
@@ -335,7 +335,7 @@ class MarketAtomCommonServiceImpl : MarketAtomCommonService {
         val taskDataMap = try {
             JsonUtil.toMap(taskJsonStr)
         } catch (e: Exception) {
-            logger.debug("parseBaseTaskJson fail $e")
+            logger.debug("parseBaseTaskJson fail $e | $taskJsonStr")
             throw ErrorCodeException(
                 errorCode = StoreMessageCode.USER_ATOM_CONF_INVALID,
                 params = arrayOf("$TASK_JSON_NAME is parseBaseTaskJson")
