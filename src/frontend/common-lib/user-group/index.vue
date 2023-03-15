b2699ac0b813f7f06bb8818905ed7f7cf74ac1a1
vhwweng <1010382269@qq.com>
2023-03-15 10:57:03 +0800

    feature: 添加项目管理前端服务 issue #7923

--- src/frontend/common-lib/user-group/index.vue
+++ src/frontend/common-lib/user-group/index.vue
@@ -118,6 +118,12 @@
             },
             isOpenManageLoading: {
                 type: Boolean
+            },
+            projectName: {
+                type: String
+            },
+            groupName: {
+                type: String
             }
         },
 
