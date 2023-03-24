191b87c119f16bae91446b719345e51f99b9ab95
vhwweng <1010382269@qq.com>
2023-03-24 21:25:26 +0800

    feature: 添加项目管理前端服务 issue #7923

--- src/frontend/devops-nav/src/components/Header/index.vue
+++ src/frontend/devops-nav/src/components/Header/index.vue
@@ -47,7 +47,7 @@
                                 class="bk-selector-create-item"
                                 @click.stop.prevent="handleApplyProject"
                             >
-                                <logo name="appyl" size="14" />
+                                <logo name="apply" size="14" />
                                 <span class="text">{{ $t('applyProject') }}</span>
                             </span>
                             <span class="extension-line" />
