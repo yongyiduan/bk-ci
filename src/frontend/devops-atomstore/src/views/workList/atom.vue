e7a55eec66cd0f68d018e1f68621a512e7f6ea5e
lockiechen <33082528+lockiechen@users.noreply.github.com>
2023-04-18 12:17:02 +0800

    hotfix: 研发商店国际化处理 issue #7794

--- src/frontend/devops-atomstore/src/views/workList/atom.vue
+++ src/frontend/devops-atomstore/src/views/workList/atom.vue
@@ -151,7 +151,7 @@
 
                     </div>
                     <div class="bk-form-item is-required">
-                        <label class="bk-label"> {{ $t('store.标识：') }} </label>
+                        <label class="bk-label"> {{ $t('store.标识') }} </label>
                         <div class="bk-form-content atom-item-content is-tooltips">
                             <div style="min-width: 100%;">
                                 <bk-input class="atom-id-input" :placeholder="$t('store.请输入英文名称，不超过30个字符')"
@@ -174,15 +174,9 @@
                                 </template>
                             </bk-popover>
                         </div>
-                        <bk-popover placement="right">
-                            <i class="devops-icon icon-info-circle"></i>
-                            <template slot="content">
-                                <p> {{ $t('store.由汉字、英文字母、数字、连字符、下划线或点组成，不超过40个字符') }} </p>
-                            </template>
-                        </bk-popover>
                     </div>
                     <div class="bk-form-item is-required">
-                        <label class="bk-label"> {{ $t('store.调试项目：') }} </label>
+                        <label class="bk-label"> {{ $t('store.调试项目') }} </label>
                         <div class="bk-form-content atom-item-content is-tooltips">
                             <div style="min-width: 100%">
                                 <bk-select
