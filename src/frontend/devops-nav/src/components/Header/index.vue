7180d659c3a5b0bc7f5d2810751e904483501fa1
vhwweng <1010382269@qq.com>
2023-04-27 11:11:05 +0800

    feature: 添加项目管理前端服务  issue #7923

--- src/frontend/devops-nav/src/components/Header/index.vue
+++ src/frontend/devops-nav/src/components/Header/index.vue
@@ -39,11 +39,12 @@
                                     {{ item.projectName }}
                                 </div>
                                 <span
+                                    v-if="item.showUserManageIcon"
                                     class="bk-selector-create-item"
                                     v-bk-tooltips="$t('userManage')"
                                     @click.stop.prevent="goToUserManage(item.managePermission)">
                                     <icon
-                                        :name="item.managePermission ? 'icon-user-manage' : 'icon-user-manage-disabled'"
+                                        :name="item.managePermission ? 'user-manage' : 'user-manage-disabled'"
                                         size="14"
                                         :class="{
                                             'user-manaeg-icon': true,
